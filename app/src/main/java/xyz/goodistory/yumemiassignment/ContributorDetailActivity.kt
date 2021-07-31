2d5a87c2e16c3bd05f169b79e01a6186df056dbc
KatoShunsuke <suke.shun.kato2@gmail.com>
2021-07-31 14:41:58 +0900

    feat: ひとまずコントリビューター詳細のアクティビティを作成

---
+++ app/src/main/java/xyz/goodistory/yumemiassignment/ContributorDetailActivity.kt
@@ -0,0 +1,11 @@
+package xyz.goodistory.yumemiassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContributorDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contributor_detail)
+    }
+}
